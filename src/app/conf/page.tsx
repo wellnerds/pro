ea26b9d03b9cbd0c68dd1f1d92e3a26b8d7fce8c
Wellington Ferreira <well_jua@hotmail.com>
2024-09-24 19:39:59 +0000

    ambiente create next-app

---
+++ src/app/conf/page.tsx
@@ -0,0 +1,8 @@
+export default function Conf() {
+    return (
+      <main>
+        <h1>Rota de pagina</h1>
+      </main>
+    );
+  }
+  
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Ambiente de Test Next.js</h1>
+    </main>
+  );
+}
